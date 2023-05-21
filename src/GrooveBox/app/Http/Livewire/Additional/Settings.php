f1ccffb43458eb572222e55c5559526bd235d7cf
Tomás Antela Rizzo <tomasnahuelantela@gmail.com>
2023-05-21 22:24:17 +0100

    Settings Component close #23

--- src/GrooveBox/app/Http/Livewire/Additional/Settings.php
+++ src/GrooveBox/app/Http/Livewire/Additional/Settings.php
@@ -2,12 +2,74 @@
 
 namespace App\Http\Livewire\Additional;
 
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Settings extends Component
 {
+
+    public $name, $username, $first_lastname, $second_lastname, $email, $password, $confirm_password, $image;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'name' => 'nullable|string|max:20',
+        'username' => 'nullable|string|unique:users,username',
+        'first_lastname' => 'nullable|string',
+        'second_lastname' => 'nullable|string',
+        'email' => 'nullable|email|unique:users,email',
+        'password' => 'nullable|string|min:6',
+        'image' => 'nullable|image',
+    ];
     public function render()
     {
         return view('livewire.additional.settings');
+    }
+
+    public function save() {
+
+        $this->validate();
+
+        $user = auth()->user();
+
+        if ($this->name) {
+            $user->name = $this->name;
+        }
+
+        if ($this->username) {
+            $user->username = $this->username;
+        }
+
+        if ($this->first_lastname) {
+            $user->lastname_first = $this->first_lastname;
+        }
+
+        if ($this->second_lastname) {
+            $user->lastname_second = $this->second_lastname;
+        }
+
+        if ($this->email) {
+            $user->email = $this->email;
+        }
+
+        if ($this->image) {
+            $newImageName = time() . str_replace(" ", "", $this->image->getClientOriginalName());
+
+            $this->image->storeAs('public/profile', $newImageName);
+
+            $path = 'profile/'.$newImageName;
+
+            $user->image = $path;
+        }
+
+        if ($this->password || $this->confirm_password) {
+            if ($this->password == $this->confirm_password) {
+                $user->password = Hash::make($this->password);
+            }
+        }
+
+        $user->save();
+        return redirect('/home');
     }
 }
--- src/GrooveBox/resources/views/livewire/additional/settings.blade.php
+++ src/GrooveBox/resources/views/livewire/additional/settings.blade.php
@@ -1,11 +1,101 @@
 <div>
-    @extends('layouts.app')
-
     @section('title')
         Settings
     @endsection
+    <div>
+        <div class="text-white fs-1 d-flex justify-content-center">
+            Settings
+        </div>
 
-    @section('content')
+        <div class="p-sm-3 p-md-5">
+            @csrf
+            <div class="row my-2">
+                <div class="col-md-4 col-sm-12">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="John" wire:model="name">
+                        @error('name')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
 
-    @endsection
+                <div class="col-md-4 col-sm-12">
+                    <div class="mb-3">
+                        <label for="flastname" class="form-label">First Lastname</label>
+                        <input type="text" class="form-control" id="flastname" placeholder="Doe" wire:model="first_lastname">
+                        @error('first_name')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="mb-3">
+                        <label for="slastname" class="form-label">Second Lastname</label>
+                        <input type="text" class="form-control" id="slastname" placeholder="Macintosh" wire:model="second_lastname">
+                        @error('second_name')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+
+            <div class="row my-2">
+                <div class="col-md-4 col-sm-12">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="email" placeholder="JohnDoe@example.test" wire:model="email">
+                        @error('email')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="sRtk6X32" wire:model="password">
+                        @error('password')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="sRtk6X32" wire:model="confirm_password">
+                    </div>
+                </div>
+            </div>
+            <div class="row my-2">
+                <div class="col-md-6 col-sm-12">
+                    <div class="mb-4">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" placeholder="JohnDoe777" wire:model="username">
+                        @error('username')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <div class="mb-4">
+                        <label for="profile-photo" class="form-label">Profile Image</label>
+                        <input type="file" class="form-control" id="profile-photo" placeholder="John" wire:model="image">
+                        @error('image')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+
+            <div class="row my-2">
+                <div class="col d-flex justify-content-center">
+                    <button wire:click="save" class="btn btn-primary btn-lg w-100">Update Profile</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
